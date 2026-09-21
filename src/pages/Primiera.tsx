561e0de645ac8c795ee9e46ecbcc58d92ab09785
Kim Robinson <kimmykokonut@gmail.com>
2026-09-20 18:00:48 -0700

    remove hidden overflow from primiera calc

--- src/pages/Primiera.tsx
+++ src/pages/Primiera.tsx
@@ -2,7 +2,7 @@ import PrimieraCalculator from "../components/PrimieraCalculator";
 
 function Primiera() {
   return (
-    <div className="w-full flex-1 flex flex-col overflow-hidden">
+    <div className="w-full flex-1 flex flex-col min-h-0">
       <PrimieraCalculator mode="page" />
     </div>
   );
